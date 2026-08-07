ed5d86ac5c4445f6999ae78a16e750ddbc45402a
Ezheriell <dioazril@gmail.com>
2026-08-07 08:49:46 +0700

    chore: ganti flutter_markdown (discontinued) ke flutter_markdown_plus

--- lib/ui/widgets/markdown_helpers.dart
+++ lib/ui/widgets/markdown_helpers.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:markit/ui/theme/palette.dart';
 import 'package:markit/ui/theme/typography.dart';
 
